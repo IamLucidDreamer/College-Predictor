263b605ee123873acbc68673ce5fbe91184f10c9
krish7x <krishnakumar99g@gmail.com>
2023-03-24 22:04:26 +0530

    feat: add blog and updates with pagination

---
+++ src/controllers/updates.js
@@ -0,0 +1,129 @@
+const { create, updateById, deleteById, getById } = require('../helpers/crud')
+const updatesSchema = require('../models/updates')
+const { statusCode: SC } = require('../utils/statusCode')
+const { loggerUtil: logger } = require('../utils/logger')
+
+const createUpdates = async (req, res) => {
+	try {
+		await create(req.body, updatesSchema)
+			.then(data => {
+				res.status(SC.OK).json(data)
+			})
+			.catch(err => {
+				res.status(SC.INTERNAL_SERVER_ERROR).json({
+					status: 'Failed!',
+					err
+				})
+			})
+	} catch (err) {
+		loggerUtil(err, 'ERROR')
+	} finally {
+		logger('Create Updates Function is Executed')
+	}
+}
+
+const updateUpdates = async (req, res) => {
+	try {
+		const id = req.params.id
+		await updateById(req.body, id, updatesSchema)
+			.then(data => {
+				res.status(SC.OK).json(data)
+			})
+			.catch(err => {
+				res.status(SC.INTERNAL_SERVER_ERROR).json({
+					status: 'Failed!',
+					err
+				})
+			})
+	} catch (err) {
+		loggerUtil(err, 'ERROR')
+	} finally {
+		logger('Update Updates Function is Executed')
+	}
+}
+
+const deleteUpdates = async (req, res) => {
+	try {
+		const id = req.params.id
+		await deleteById(id, updatesSchema)
+			.then(data => {
+				res.status(SC.OK).json(data)
+			})
+			.catch(err => {
+				res.status(SC.INTERNAL_SERVER_ERROR).json({
+					status: 'Failed!',
+					err
+				})
+			})
+	} catch (err) {
+		loggerUtil(err, 'ERROR')
+	} finally {
+		logger('Delete Updates Function is Executed')
+	}
+}
+
+const getUpdates = async (req, res) => {
+	const id = req.params.id
+	try {
+		await getById(id, updatesSchema)
+			.then(data => {
+				res.status(SC.OK).json(data)
+			})
+			.catch(err => {
+				res.status(SC.INTERNAL_SERVER_ERROR).json({
+					status: 'Failed!',
+					err
+				})
+			})
+	} catch (err) {
+		loggerUtil(err, 'ERROR')
+	} finally {
+		logger('Get Updates Function is Executed')
+	}
+}
+const label = {
+	totalDocs: 'totalupdatess',
+	docs: 'updatess',
+	limit: 'perPage',
+	page: 'currentPage',
+	nextPage: 'nextPageNo',
+	prevPage: 'prevPageNo',
+	totalPages: 'pageCount'
+}
+
+const getAllUpdates = async (req, res) => {
+	const options = {
+		page: 1,
+		limit: 10,
+		customLabels: label
+	}
+	req.query.page !== undefined ? (options.page = req.query.page) : null
+	req.query.limit !== undefined ? (options.limit = req.query.limit) : null
+	const id = req.params.id
+	try {
+		await updatesSchema.paginate({}, options, (err, result) => {
+			if (err) {
+				res.status(SC.BAD_REQUEST).json({
+					error: 'Getting updatess from DB is failed!'
+				})
+				logger(err, 'ERROR')
+			}
+			res.status(SC.OK).send({
+				message: 'updatess fetched successfully',
+				data: result
+			})
+		})
+	} catch (err) {
+		loggerUtil(err, 'ERROR')
+	} finally {
+		logger('Get all Updates Function is Executed')
+	}
+}
+
+module.exports = {
+	createUpdates,
+	updateUpdates,
+	deleteUpdates,
+	getUpdates,
+	getAllUpdates
+}
---
+++ src/models/updates.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate-v2')
+
+const updatesSchema = new mongoose.Schema(
+	{
+		userId: {
+			type: mongoose.Schema.ObjectId,
+			required: true
+		},
+		title: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		description: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		image: {
+			type: String
+		},
+		document: {
+			type: String
+		}
+	},
+	{ timestamps: true }
+)
+
+updatesSchema.plugin(mongoosePaginate)
+
+module.exports = mongoose.model('updates', updatesSchema, 'updates')
