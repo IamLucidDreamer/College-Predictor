28484c27603aa9c2085649e4c1e7cdc56a6e2afe
krish7x <krishnakumar99g@gmail.com>
2023-04-01 22:00:21 +0530

    feat(college): initial commit

---
+++ src/models/college.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate-v2')
+
+const collegeSchema = new mongoose.Schema(
+	{
+		collegeTag: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		collegeName: {
+			type: String,
+			required: true,
+			trim: true
+		},
+		collegeType: {
+			type: String,
+			trim: true
+		},
+		estYear: {
+			type: Number
+		},
+		coursesOffered: {
+			type: Array
+		},
+		ranking: {
+			type: Number
+		},
+		cutOff: {
+			type: String,
+			trim: true
+		},
+		tutionFees: {
+			type: Number
+		},
+		hostelFees: {
+			type: Number
+		},
+		campusPhotos: {
+			type: Array
+		},
+		website: {
+			type: String,
+			trim: true
+		},
+		contactNumber: {
+			type: Number
+		}
+	},
+	{ timestamps: true }
+)
+
+collegeSchema.plugin(mongoosePaginate)
+module.exports = mongoose.model('neet', collegeSchema, 'neet')
