3aac22a9fdf11996b2f18d69d7d36d7066ee48e8
Manas <shuklamanasofficial@gmail.com>
2023-06-15 18:12:07 +0530

    Feat : Statistics

--- src/controllers/statistics.js
+++ src/controllers/statistics.js
@@ -2,6 +2,8 @@ const { statusCode: SC } = require('../utils/statusCode')
 const { loggerUtil: logger } = require('../utils/logger')
 const userModel = require('../models/user')
 const collegeModel = require("../models/college")
+const updatesModel = require("../models/updates")
+const blogModel = require("../models/blog")
 
 const mainData = async (req, res) => {
     try {
@@ -10,11 +12,15 @@ const mainData = async (req, res) => {
             collegeCount: 0,
             councelorCount: 0,
             collegeAdminCount: 0,
+            totalUpdatesCount: 0,
+            totalBlogsCount: 0,
         }
-        responseObj.userCount =  await userModel.count({ role: 1 })
+        responseObj.userCount = await userModel.count({ role: 1 })
         responseObj.collegeCount = await collegeModel.count({})
-        responseObj.councelorCount =  await userModel.count({ role: 2 })
-        responseObj.councelorCount =  await userModel.count({ role: 4 })
+        responseObj.councelorCount = await userModel.count({ role: 2 })
+        responseObj.councelorCount = await userModel.count({ role: 4 })
+        responseObj.totalUpdatesCount = await updatesModel.count({})
+        responseObj.totalBlogsCount = await blogModel.count({})
         res.status(SC.OK).json({
             status: SC.OK,
             data: responseObj
