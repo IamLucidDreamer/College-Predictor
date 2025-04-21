b0e7dbc80f7218acffcc6fcae75aa3ecfb0ac432
Manas Shukla <shuklamanasofficial@gmail.com>
2025-04-21 20:29:43 +0530

    Feat :minor user update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -24,30 +24,47 @@ const getUserById = async (req, res) => {
 
 const getAllUsers = async (req, res) => {
 	try {
-		const id = req.auth._id
-		const getAll = req.query.getAll
-		const isAdmin =req.query.isAdmin
-		const filter = isAdmin == 1 ? {} : getAll == 1 ? {reviewerId: null}: { reviewerId: id }
+		const id = req.auth._id;
+		const getAll = req.query.getAll;
+		const isAdmin = req.query.isAdmin;
+
+		const filter = isAdmin == 1
+			? {}
+			: getAll == 1
+			? { reviewerId: null }
+			: { reviewerId: id };
+
+		const cutoffDate = new Date('2025-01-01');
+
+		const updatedFilter = {
+			...filter,
+			$or: [
+				{ role: { $ne: 1 }, createdAt: { $lt: cutoffDate } },
+				{ createdAt: { $gte: cutoffDate } }
+			]
+		};
+
 		userModel
-			.find(filter)
+			.find(updatedFilter)
 			.sort({ createdAt: -1 })
-			.exec((err, user) => {
-				if (err || !user) {
+			.exec((err, users) => {
+				if (err || !users) {
 					return res.status(SC.NOT_FOUND).json({
-						error: 'No users were found in a DB!'
-					})
+						error: 'No users were found in the DB!'
+					});
 				}
 				res.status(SC.OK).json({
-					message: 'User Fetched Successfully!',
-					data: user
-				})
-			})
+					message: 'Users fetched successfully!',
+					data: users
+				});
+			});
 	} catch (err) {
-		logger(err, 'ERROR')
+		logger(err, 'ERROR');
 	} finally {
-		logger('Get All Users Function is Executed')
+		logger('Get All Users Function is Executed');
 	}
-}
+};
+
 
 module.exports = {
 	getUserById,
